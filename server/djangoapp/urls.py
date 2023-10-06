da7a6eccb082a46499412b1ab74466a22cae642b
Mudalue <emekachristian511@gmail.com>
2023-10-06 14:26:02 -0400

    authentication flow

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,16 +10,19 @@ urlpatterns = [
     # name the URL
 
     # path for about view
-   path(route='', view=views.index, name='index'),
-   path(route = 'about/', view = views.about, name='about'),
-   path(route = 'contact/', view = views.contact, name='contact'),
+    path(route='', view=views.index, name='index'),
+
+    path(route = 'about/', view = views.about, name='about'),
+
+    path(route = 'contact/', view = views.contact, name='contact'),
     # path for contact us view
 
     # path for registration
-
+    path('signup/', views.registration_request, name='registration'),
     # path for login
-
+    path('login/', views.login_request, name='login'),
     # path for logout
+    path('logout/', views.logout_request, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
 
